556225274a3b85eaff138ae0e915493b202e1862
PascalSeth <pascalelikem@gmail.com>
2024-07-17 10:09:15 +0000

    np img

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pascal's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "Modern & Minimal Portfolio",
 };
 
 export default function RootLayout({
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
