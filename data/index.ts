6d1a85d1848c979deed37b0a02bb9cbd0ff02dd6
PascalSeth <pascalelikem@gmail.com>
2024-07-17 09:47:20 +0000

    push

---
+++ data/index.ts
@@ -0,0 +1,250 @@
+export const navItems = [
+  { name: "Home", link: "#" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Tranzbook",
+    des: "Transportation services in Ghana",
+    img: "/projects/tranzbook.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://tranzbook.netlify.app/"
+  },
+  {
+    id: 2,
+    title: "Acer Landing Page",
+    des: "Transportation services in Ghana",
+    img: "/projects/acer.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg",],
+    link: "https://pascalseth.github.io/acerlanding/#"
+  },
+  {
+    id: 3,
+    title: "Grand Canyon Landing Page",
+    des: "Grand Canyon Landing",
+    img: "/projects/grandcanyon.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://pascalseth.github.io/grandcanyon/"
+  },
+  {
+    id: 4,
+    title: "Travel Landing Page",
+    des: "Travel Agent Landing Page",
+    img: "/projects/travelAgent.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg",],
+    link: "https://main--travelguidets.netlify.app/"
+  },
+  {
+    id: 5,
+    title: "Admin Dashboard",
+    des: "Admin Dashboard Landing Page",
+    img: "/projects/admin.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://mycleanapp.netlify.app/"
+  },
+  {
+    id: 6,
+    title: "E-commerce",
+    des: "E-commerce",
+    img: "/projects/ecommerce.png",
+    iconLists: ["/re.svg", "/tail.svg", ],
+    link: "https://ecommerceshop-zeta.vercel.app/"
+  },
+  {
+    id: 7,
+    title: "AgriKeeper",
+    des: "AgriKeeper E-commerce",
+    img: "/projects/akrikeep.png",
+    iconLists: ["/re.svg", "/tail.svg", ],
+    link: "https://afrikeep.vercel.app/"
+  },
+  {
+    id: 8,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com"
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Pascal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Pascal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Pascal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Pascal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Pascal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Pascal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Pascal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Pascal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Pascal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Pascal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Pascal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Pascal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Pascal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Pascal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Pascal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
